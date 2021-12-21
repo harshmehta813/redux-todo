dd893c47e02bc9965bae5e728f761986bf298e57
Harsh Mehta <harshmehta813@gmail.com>
2021-12-21 22:01:29 +0530

    added multiple middlewares

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,37 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
 // import reducer from "./reducer";
 import authReducer from "./auth/reducer";
 import appReducer from "./app/reducer";
 
 const rootReducer = combineReducers({ auth: authReducer, app: appReducer });
 
+const loggerMiddleware = (store) => (next) => (action) => {
+  console.log("dispatching the action in 1", action);
+  console.log(store.getState());
+  const value = next(action);
+  console.log(value);
+  console.log(store.getState());
+  console.log("end of 1");
+};
+
+// 2nd middleware
+
+const loggerMiddleware2 = (store) => (next) => (action) => {
+  console.log("dispatching the action in 2", action);
+  console.log(store.getState());
+  const value = next(action);
+  console.log(value);
+  console.log(store.getState());
+  console.log("end of 2");
+};
+
 export const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(loggerMiddleware, loggerMiddleware2)
 );
+
+// loggerMiddleware(store)(nextMiddleware)(action)
 
 console.log(store.getState());
 // * combine reducers
